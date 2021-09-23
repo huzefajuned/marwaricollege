bd76f1de085d5f6be7f4f3e065eebad0857c9c01
Osama bin junaid <osamabinjunaid@Osamas-MacBook-Pro.local>
2021-09-23 10:00:02 +0530

    javascript file add

---
+++ style.js
@@ -0,0 +1,6 @@
+function popup(){
+    var onscreen="WELCOME TO THE OFFICIAL WEBSITE OF MARWARI COLLEGE KISHANGANJ, please click on MARWARI COLLEGE (logo) and fill the Form .If You are a student of marwari college, THANK U ALL!";
+
+    alert(onscreen);
+}
+popup();
